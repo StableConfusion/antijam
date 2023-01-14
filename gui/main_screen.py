cfbb9fda467b6f8002262dcf425598509e35938d
Arch4ngel21 <adammichal657@gmail.com>
2023-01-14 17:22:57 +0100

    Added roads and cars

--- gui/main_screen.py
+++ gui/main_screen.py
@@ -2,38 +2,68 @@ import numpy as np
 import pygame
 import display_settings
 
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
-from gui.resource_manager import ResourceManager
+from gui.resource_manager import ResourceManager, rotate_resize_car, rotate_road
+from environment import Vehicle, Junction, Direction
 
 
 class MainScreen:
     def __init__(self, town_map: np.ndarray, width: int, height: int):
         pygame.init()
-        self.town_map = town_map
-        self.map_height, self.map_width = self.town_map.shape
+        self.parse_town_map(town_map)
+        self.map_height, self.map_width = town_map.shape
         self.width = width
         self.height = height
+
         self.screen: pygame.Surface = pygame.display.set_mode((width, height))
         self.resource_manager = ResourceManager()
+
+        self.vehicle_state: Optional[List[Vehicle]] = None
+        self.junction_state: Optional[List[Junction]] = None
+        self.vehicle_state_tmp: Optional[List[Junction]] = None
+        self.junction_state_tmp: Optional[List[Junction]] = None
+        self.should_update = False
+
         self.clock = pygame.time.Clock()
         self.running = True
 
-    def render_map(
-            self,
-            vehicles_state: List[Tuple],
-            junctions_state: List[Tuple]
-    ):
+    def render_map(self):
         for y in range(self.map_height):
             for x in range(self.map_width):
-                if not self.town_map[y, x]:
+                if self.town_map[y][x] == "grass":
                     self.screen.blit(self.resource_manager.grass_tile, (x * display_settings.DEFAULT_TILE_SIZE, y * display_settings.DEFAULT_TILE_SIZE))
                 else:
                     self.screen.blit(self.resource_manager.soil_tile, (x * display_settings.DEFAULT_TILE_SIZE, y * display_settings.DEFAULT_TILE_SIZE))
+                    self.screen.blit(rotate_road(self.town_map[y][x], self.resource_manager),
+                                     (x * display_settings.DEFAULT_TILE_SIZE, y * display_settings.DEFAULT_TILE_SIZE))
+        if self.vehicle_state is not None:
+            for vehicle in self.vehicle_state:
+                y = vehicle.i
+                x = vehicle.j
+
+                self.screen.blit(rotate_resize_car(self.resource_manager.car_tiles[1], vehicle.direction),
+                                 (x * display_settings.DEFAULT_TILE_SIZE, y * display_settings.DEFAULT_TILE_SIZE))
+
+        if self.junction_state is not None:
+            pass
 
         pygame.display.flip()
 
+    def step(self, vehicle_state: List[Vehicle], junction_state: List[Junction]):
+        self.vehicle_state_tmp = vehicle_state
+        self.junction_state_tmp = junction_state
+        self.should_update = True
+
+    def update_state(self):
+        if self.should_update:
+            self.vehicle_state = self.vehicle_state_tmp.copy()
+            self.junction_state = self.junction_state_tmp.copy()
+            self.should_update = False
+
     def run(self):
+        self.vehicle_state = [Vehicle(None, 1, 2, Direction.LEFT), Vehicle(None, 4, 7, Direction.UP)]
+
         while self.running:
             self.clock.tick(60)
 
@@ -41,7 +71,45 @@ class MainScreen:
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            self.render_map(None, None)
+            self.update_state()
+            self.render_map()
+
+    def parse_town_map(self, town_map: np.ndarray):
+        self.town_map = [[None for _ in range(town_map.shape[1])] for _ in range(town_map.shape[0])]
+
+        for y in range(town_map.shape[0]):
+            for x in range(town_map.shape[1]):
+                if not town_map[y, x]:
+                    self.town_map[y][x] = "grass"
+                else:
+                    if not town_map[y-1][x] and not town_map[y-1][x-1] and not town_map[y][x-1]:
+                        self.town_map[y][x] = "road_corner_right_down"
+                    elif not town_map[y][x+1] and not town_map[y-1][x+1] and not town_map[y-1][x]:
+                        self.town_map[y][x] = "road_corner_left_down"
+                    elif not town_map[y+1][x] and not town_map[y+1][x+1] and not town_map[y][x+1]:
+                        self.town_map[y][x] = "road_corner_left_up"
+                    elif not town_map[y][x-1] and not town_map[y+1][x-1] and not town_map[y+1][x]:
+                        self.town_map[y][x] = "road_corner_right_up"
+                    elif (not town_map[y-1][x-1] and not town_map[y-1][x]) or (not town_map[y-1][x] and not town_map[y-1][x+1]):
+                        self.town_map[y][x] = "road_corner_straight_right"
+                    elif (not town_map[y-1][x+1] and not town_map[y][x+1]) or (not town_map[y][x+1] and not town_map[y+1][x+1]):
+                        self.town_map[y][x] = "road_corner_straight_down"
+                    elif (not town_map[y+1][x-1] and not town_map[y+1][x]) or (not town_map[y+1][x] and not town_map[y+1][x+1]):
+                        self.town_map[y][x] = "road_corner_straight_left"
+                    elif (not town_map[y-1][x-1] and not town_map[y][x-1]) or (not town_map[y][x-1] and not town_map[y+1][x-1]):
+                        self.town_map[y][x] = "road_corner_straight_up"
+                    elif not town_map[y+1][x+1]:
+                        self.town_map[y][x] = "road_corner_right_down"
+                    elif not town_map[y+1][x-1]:
+                        self.town_map[y][x] = "road_corner_left_down"
+                    elif not town_map[y-1][x-1]:
+                        self.town_map[y][x] = "road_corner_left_up"
+                    elif not town_map[y-1][x+1]:
+                        self.town_map[y][x] = "road_corner_right_up"
+                    else:
+                        print(f"Error in parsing map! x = {x}, y = {y}")
+                        exit(-1)
+        print(self.town_map[2][6])
 
 
 if __name__ == '__main__':
--- gui/resource_manager.py
+++ gui/resource_manager.py
@@ -5,6 +5,8 @@ import numpy as np
 import pygame
 
 import display_settings
+from environment import Direction
+
 
 class TileSet:
     def __init__(self, file, size=(32, 32), margin=1, spacing=1):
@@ -78,6 +80,19 @@ def scale_2_car_size(image):
     return pygame.transform.scale(image, (display_settings.DEFAULT_CAR_WIDTH, display_settings.DEFAULT_TILE_SIZE))
 
 
+def rotate_resize_car(image, direction: Direction):
+    if direction == Direction.RIGHT:
+        return pygame.transform.scale(pygame.transform.rotate(image, 270),
+                                      (display_settings.DEFAULT_TILE_SIZE, display_settings.DEFAULT_CAR_WIDTH))
+    elif direction == Direction.DOWN:
+        return pygame.transform.rotate(image, 180)
+    elif direction == Direction.LEFT:
+        return pygame.transform.scale(pygame.transform.rotate(image, 90),
+                                      (display_settings.DEFAULT_TILE_SIZE, display_settings.DEFAULT_CAR_WIDTH))
+    else:
+        return image
+
+
 class ResourceManager:
     """
     Image set author: craftpix (Free Game Assets (GUI, Sprite, Tilesets)
@@ -94,10 +109,16 @@ class ResourceManager:
         for tile_image in [f for f in listdir(resource_path_road_1) if isfile(join(resource_path_road_1, f))]:
             self.road_1_tiles.append(scale_2_tile_size(pygame.image.load(join(resource_path_road_1, tile_image))))
 
+        self.road_1_tiles[0] = pygame.transform.scale(self.road_1_tiles[0], (display_settings.DEFAULT_TILE_SIZE + 5, display_settings.DEFAULT_TILE_SIZE + 5))
+        self.road_1_tiles[1] = pygame.transform.scale(self.road_1_tiles[1], (display_settings.DEFAULT_TILE_SIZE + 5, display_settings.DEFAULT_TILE_SIZE + 5))
+
         # Road 2 Tiles
         self.road_2_tiles = []
         for tile_image in [f for f in listdir(resource_path_road_2) if isfile(join(resource_path_road_2, f))]:
             self.road_2_tiles.append(scale_2_tile_size(pygame.image.load(join(resource_path_road_2, tile_image))))
+
+        self.road_2_tiles[0] = pygame.transform.scale(self.road_2_tiles[0], (display_settings.DEFAULT_TILE_SIZE + 5, display_settings.DEFAULT_TILE_SIZE + 5))
+        self.road_2_tiles[1] = pygame.transform.scale(self.road_2_tiles[1], (display_settings.DEFAULT_TILE_SIZE + 5, display_settings.DEFAULT_TILE_SIZE + 5))
 
         # Background Tiles
         self.grass_tile = scale_2_tile_size(pygame.image.load(join(resource_path_background, "grass.png")))
@@ -109,3 +130,30 @@ class ResourceManager:
         for tile_image in [f for f in listdir(resource_path_car) if isfile(join(resource_path_car, f))]:
             self.car_tiles.append(scale_2_car_size(pygame.image.load(join(resource_path_car, tile_image))))
 
+
+def rotate_road(road_type, resource_manager: ResourceManager):
+    """
+    :param image: image to rotate
+    :param road_type: type of road tile
+    :param resource_manager: instance of ResourceManager
+    :return: rotated image
+    """
+
+    if road_type == "road_corner_right_down":
+        return resource_manager.road_1_tiles[1]
+    elif road_type == "road_corner_left_down":
+        return pygame.transform.rotate(resource_manager.road_1_tiles[1], -90)
+    elif road_type == "road_corner_left_up":
+        return pygame.transform.rotate(resource_manager.road_1_tiles[0], -270)
+    elif road_type == "road_corner_right_up":
+        return resource_manager.road_1_tiles[0]
+    elif road_type == "road_corner_straight_right":
+        return resource_manager.road_1_tiles[2]
+    elif road_type == "road_corner_straight_down":
+        return pygame.transform.rotate(resource_manager.road_1_tiles[2], 90)
+    elif road_type == "road_corner_straight_left":
+        return resource_manager.road_1_tiles[2]
+    elif road_type == "road_corner_straight_up":
+        return pygame.transform.rotate(resource_manager.road_1_tiles[2], 90)
+    else:
+        raise ValueError
