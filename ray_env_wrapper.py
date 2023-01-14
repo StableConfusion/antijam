4d68acece98bfdf88884e80dcff50dc4c1242160
Carbon225 <carbon225@protonmail.com>
2023-01-14 17:47:05 +0100

    it trains

--- ray_env_wrapper.py
+++ ray_env_wrapper.py
@@ -1,25 +1,39 @@
+import numpy as np
 from gym.spaces import Discrete, Box
 from ray.rllib.env import MultiAgentEnv, EnvContext
+from environment import GridWorldEnv
 
 
 class AntiJamEnv(MultiAgentEnv):
     def __init__(self, config: EnvContext):
-        self.num_lights: int = config["num_lights"]
-        self.grid_size: tuple[int, int] = config["grid_size"]
-        self.action_space = {
-            f"light_{i}": Discrete(2) for i in range(self.num_lights)
-        }
-        self.observation_space = {
-            f"light_{i}": Box(
-                low=0,
-                high=1,
-                shape=(self.grid_size[0], self.grid_size[1], 5),
-                dtype="uint8",
-            ) for i in range(self.num_lights)
-        }
+        super().__init__()
+
+        self.steps_left = 100
+
+        self.env = GridWorldEnv()
+
+        self._agent_ids = [f"light_{i}" for i in range(len(self.env.junctions))]
+
+        self.num_lights: int = len(self.env.junctions)
+        self.num_cars: int = len(self.env.vehicles)
+        self.grid_size: tuple[int, int] = self.env.map.shape
+
+        self.action_space = Discrete(2)
+
+        self.observation_space = Box(
+            low=0,
+            high=1,
+            shape=(6 * self.grid_size[0] * self.grid_size[1],),
+            dtype=np.uint8,
+        )
 
     def reset(self):
-        pass
+        self.env = GridWorldEnv()
+        self.steps_left = 100
+        obs_dict = {}
+        for i in range(self.num_lights):
+            obs_dict[f"light_{i}"] = self.get_light_observation(i)
+        return obs_dict
 
     def step(self, action_dict):
         # action_dict: {"light_{i}": 0 or 1}
@@ -28,7 +42,60 @@ class AntiJamEnv(MultiAgentEnv):
         #     reward_dict: {"light_{i}": mean car speed}
         #     done_dict: {"light_{i}": False, "__all__": False}
         #     info_dict: {"light_{i}": {}}
-        pass
+        obs_dict = {}
+        reward_dict = {}
+        done_dict = {}
+        info_dict = {}
 
-    def render(self, mode='human'):
-        pass
+        for i in range(self.num_lights):
+            self.env.junctions[i].state = action_dict[f"light_{i}"]
+
+        num_moved = self.env.step()
+        reward = num_moved / self.num_cars
+
+        self.steps_left -= 1
+        done = self.steps_left <= 0
+
+        for i in range(self.num_lights):
+            obs_dict[f"light_{i}"] = self.get_light_observation(i)
+            reward_dict[f"light_{i}"] = reward
+            done_dict[f"light_{i}"] = done
+            info_dict[f"light_{i}"] = {}
+
+        done_dict["__all__"] = done
+
+        return obs_dict, reward_dict, done_dict, info_dict
+
+    def get_light_observation(self, light_id):
+        # return NxMx6 tensor
+        # 0: map
+        # 1: junctions
+        # 2: cars
+        # 3: this light position
+        # 4: lights which are 0
+        # 5: lights which are 1
+        obs = np.zeros((self.grid_size[0], self.grid_size[1], 6), dtype=np.uint8)
+        
+        obs[:, :, 0] = self.env.map
+        
+        for junction in self.env.junctions:
+            obs[junction.i : junction.i + 2,
+                junction.j : junction.j + 2, 1] = 1
+
+            if junction.state == 0:
+                obs[junction.i : junction.i + 2,
+                    junction.j : junction.j + 2, 4] = 1
+
+            elif junction.state == 1:
+                obs[junction.i : junction.i + 2,
+                    junction.j : junction.j + 2, 5] = 1
+
+        for vehicle in self.env.vehicles:
+            obs[vehicle.i, vehicle.j, 2] = 1
+
+        this_junction = self.env.junctions[light_id]
+
+        obs[this_junction.i : this_junction.i + 2,
+            this_junction.j : this_junction.j + 2, 3] = 1
+
+        return obs.flatten()
--- train.py
+++ train.py
@@ -1,74 +1,41 @@
-import argparse
-import gym
-from gym.spaces import Discrete, Box
-import numpy as np
-
 import ray
-from ray import air, tune
+from ray import tune
 from ray.air.integrations.wandb import WandbLoggerCallback
-from ray.rllib.env.env_context import EnvContext
-from ray.tune.registry import get_trainable_cls
+from ray.tune.registry import register_env
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--run", type=str, default="PPO", help="The RLlib-registered algorithm to use."
-)
-parser.add_argument(
-    "--framework",
-    choices=["tf", "tf2", "torch"],
-    default="torch",
-    help="The DL framework specifier.",
-)
-
-
-class SimpleCorridor(gym.Env):
-    def __init__(self, config: EnvContext):
-        self.end_pos = config["corridor_length"]
-        self.cur_pos = 0
-        self.action_space = Discrete(2)
-        self.observation_space = Box(0.0, self.end_pos, shape=(1,), dtype=np.float32)
-
-    def reset(self):
-        self.cur_pos = 0
-        return [self.cur_pos]
-
-    def step(self, action):
-        assert action in [0, 1], action
-        if action == 0 and self.cur_pos > 0:
-            self.cur_pos -= 1
-        elif action == 1:
-            self.cur_pos += 1
-        done = self.cur_pos >= self.end_pos
-        return [self.cur_pos], 1 if done else -0.1, done, {}
+from ray_env_wrapper import AntiJamEnv
 
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-    print(f"Running with following CLI options: {args}")
-
     ray.init()
 
-    config = (
-        get_trainable_cls(args.run)
-        .get_default_config()
-        .environment(SimpleCorridor, env_config={"corridor_length": 5})
-        .framework(args.framework)
-        .rollouts(num_rollout_workers=8)
-        .resources(num_gpus=1)
-    )
+    register_env("antijam", lambda config: AntiJamEnv(config))
+    single_env = AntiJamEnv({})
+    obs_space = single_env.observation_space
+    act_space = single_env.action_space
 
-    print("Training automatically with Ray Tune")
-    tuner = tune.Tuner(
-        args.run,
-        param_space=config.to_dict(),
-        run_config=air.RunConfig(
-            local_dir='./ray_results',
-            callbacks=[
-                WandbLoggerCallback(project='antijam'),
-            ],
-        ),
+    def gen_policy(_):
+        return (None, obs_space, act_space, {})
+
+    policies = {
+        'policy_{}'.format(i): gen_policy(i) for i in range(1)
+    }
+    policy_ids = list(policies.keys())
+
+    tune.run(
+        'PPO',
+        config={
+            'env': 'antijam',
+            'framework': 'torch',
+            'num_gpus': 1,
+            'disable_env_checking': True,
+            'multiagent': {
+                'policies': policies,
+                'policy_mapping_fn': lambda agent_id, episode, worker, **kwargs: policy_ids[0],
+            },
+        },
+        callbacks=[WandbLoggerCallback(project='antijam')],
+        local_dir='./ray_results'
     )
-    results = tuner.fit()
 
     ray.shutdown()
